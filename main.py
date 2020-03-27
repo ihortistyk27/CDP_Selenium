1c454b31d4de5ee7edcb3c5385258782209f22f2
Ihor.Tistyk <ihor.tistyk@thomsonreuters.com>
2020-03-27 10:15:51 +0200

    Refactored allure name and added gitignore.

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import pytest
 pytest_args = ["C:\CDP_Selenium\\tests",
                "--disable-pytest-warnings",
                "-vv",
-               "--alluredir", "./report"]
+               "--alluredir", "/allure-report"]
 
 pytest.main(pytest_args)
