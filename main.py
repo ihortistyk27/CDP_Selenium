58e4c9091d5e2e57c78103b2df4dc16839daa07a
Ihor.Tistyk <ihor.tistyk@thomsonreuters.com>
2020-03-27 13:52:46 +0200

    Changed allure path.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pytest
 pytest_args = ["C:\CDP_Selenium\\tests",
                "--disable-pytest-warnings",
                "-vv",
-               "--alluredir", "./target/allure-results"
+               "--alluredir", "target/allure-results"
                ]
 
 pytest.main(pytest_args)
